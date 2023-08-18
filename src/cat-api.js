00541b7e2cd52e0bc43fea2d361fbfbcaf6a861e
LiudmylaHorobets <horobets1323@gmail.com>
2023-08-18 14:21:32 +0300

    fix cat-api

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const BREEDS_URL = '/breeds';
 const IMAGES_URL = '/images/search';
+const API_KEY =
+  'live_AInBiZNMGTspPJow6eVuTFhnUUqlCCv9K3ia4eMvA7myxAvI8V3zJkjb4kF1xzmt';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_NWEy6xxwLGvFpS0aqUewOl5VMamxgNKtUChLGZX6T3mgkKvBlzqLkOd3CbEfq6Bv';
@@ -11,7 +13,7 @@ export function fetchBreeds() {
   return axios
     .get(`${BASE_URL}${BREEDS_URL}`, {
       params: {
-        api_key: 'x-api-key',
+        api_key: API_KEY,
       },
     })
     .then(resp => {
@@ -29,7 +31,7 @@ export function fetchCatByBreed(breedId) {
   return axios
     .get(`${BASE_URL}${IMAGES_URL}`, {
       params: {
-        api_key: 'x-api-key',
+        api_key: API_KEY,
         breed_ids: breedId,
       },
     })
